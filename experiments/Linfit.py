3af415ca6b253d0f276e7c816cf8a96289bac1b1
QuantumJunction <alex.herrmann.ah@gmail.com>
2025-11-30 15:14:56 +0100

    added linfit

---
+++ experiments/Linfit.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import linregress
+
+targetValue     = [1.0,     4.0,    6.6788, 10.5467]
+measuredValues  = [1.2345,  4.545,  6.6788, 10.5467]
+
+regression = linregress(targetValue,measuredValues)
+
+plottingLineX = np.linspace (min(min(targetValue,measuredValues)),max(max(targetValue,measuredValues)),1000)
+plottingLineY = regression.slope * plottingLineX + regression.intercept
+
+print(f"Regression slope: {regression.slope} intercept {regression.intercept}")
+
+plt.plot(plottingLineX,plottingLineY)
+plt.plot(targetValue,measuredValues,'ro')
+plt.show()
